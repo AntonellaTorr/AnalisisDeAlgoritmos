0abb0b872f75b211b4466f5b4c326d028d8b7838
AntonellaTorr <67662917+AntonellaTorr@users.noreply.github.com>
2022-11-28 23:36:25 -0300

    Agregamos implementacion de pareja perfecta

--- TPO/Algoritmo.java
+++ TPO/Algoritmo.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Algoritmo
 
 {
-   //proponente HOMBRE
-   //propuesto MUJER
+    // proponente HOMBRE
+    // propuesto MUJER
     private int N, cantEmparejados;
     private String[][] proponentePref;
     private String[][] propuestoPref;
@@ -14,10 +14,9 @@ public class Algoritmo
     private String[] propuesto;
     private String[] propuestoPareja;
     private boolean[] proponenteEmparejado;
- 
+
     /** Constructor **/
-    public Algoritmo(String[] m, String[] w, String[][] mp, String[][] wp)
-    {
+    public Algoritmo(String[] m, String[] w, String[][] mp, String[][] wp) {
         N = mp.length;
         cantEmparejados = 0;
         proponente = m;
@@ -28,49 +27,42 @@ public class Algoritmo
         propuestoPareja = new String[N];
         calcMatches();
     }
+
     /** function to calculate all matches **/
 
-  
-    private void calcMatches()
-    {
-        while (cantEmparejados < N)
-        {
+    private void calcMatches() {
+        while (cantEmparejados < N) {
             int j;
             for (j = 0; j < N; j++)
                 if (!proponenteEmparejado[j])
                     break;
-                    System.out.println( "PROPONENTE "+ j);
-            for (int i = 0; i < N && !proponenteEmparejado[j]; i++)
-            {
-               
+            for (int i = 0; i < N && !proponenteEmparejado[j]; i++) {
+
                 int indice = propuestoObtenerIndice(proponentePref[j][i]);
-                System.out.println("EL PROPONENTE BUSCADO ES :"+ proponentePref[j][i].toString() );
-                if (propuestoPareja[indice] == null)
-                {
+                if (propuestoPareja[indice] == null) {
                     propuestoPareja[indice] = proponente[j];
                     proponenteEmparejado[j] = true;
                     cantEmparejados++;
-                    
-                }
-                else
-                {
+
+                } else {
                     String parejaActual = propuestoPareja[indice];
-                    if (mayorPreferencia(parejaActual, proponente[j], indice))
-                    {
+                    if (mayorPreferencia(parejaActual, proponente[j], indice)) {
                         propuestoPareja[indice] = proponente[j];
                         proponenteEmparejado[j] = true;
                         proponenteEmparejado[proponenteObtenerIndice(parejaActual)] = false;
                     }
                 }
-            }            
+            }
         }
-        printCouples();
+        mostrarEmparejamiento();
     }
-    /** funcion para verificar si el propuesto prefiere al proponente actual o al anterior **/
-    private boolean mayorPreferencia(String proponenteActual, String nuevoProponente, int index)
-    {
-        for (int i = 0; i < N; i++)
-        {
+
+    /**
+     * funcion para verificar si el propuesto prefiere al proponente actual o al
+     * anterior
+     **/
+    private boolean mayorPreferencia(String proponenteActual, String nuevoProponente, int index) {
+        for (int i = 0; i < N; i++) {
             if (propuestoPref[index][i].equals(nuevoProponente))
                 return true;
             if (propuestoPref[index][i].equals(proponenteActual))
@@ -78,135 +70,131 @@ public class Algoritmo
         }
         return false;
     }
-    /** get men index **/
-    private int proponenteObtenerIndice(String str)
-    {
-        for (int i = 0; i < N; i++){
+
+    /** get proponente index **/
+    private int proponenteObtenerIndice(String str) {
+        for (int i = 0; i < N; i++) {
             if (proponente[i].equals(str))
                 return i;
         }
         return -1;
     }
-    /** get women index **/
-    private int propuestoObtenerIndice(String str)
-    {
-        for (int i = 0; i < N; i++){
-          
+
+    /** get propuesto index **/
+    private int propuestoObtenerIndice(String str) {
+        for (int i = 0; i < N; i++) {
+
             if (propuesto[i].equals(str))
                 return i;
         }
         return -1;
     }
+
     /** print couples **/
-    public void printCouples()
-    {
-        System.out.println("Couples are : ");
-        for (int i = 0; i < N; i++)
-        {
-            System.out.println(propuestoPareja[i] +" "+ propuesto[i]);
+    public void mostrarEmparejamiento() {
+        System.out.println("Emparejados : ");
+        for (int i = 0; i < N; i++) {
+            System.out.println(propuestoPareja[i] + " " + propuesto[i]);
         }
     }
 
     /** main function **/
 
-    public static String [] generarRubros(int n){
-        String[] r= new String[n];
-        for (int i=0;i<n;i++){
-            
-            r[i]= "R" +(i);
+    public static String[] generarRubros(int n) {
+        String[] r = new String[n];
+        for (int i = 0; i < n; i++) {
+            r[i] = "R" + (i);
         }
         return r;
     }
-    public static String [] generarEmpleados(int n){
-        String[] emp= new String[n];
-        for (int i=0;i<n;i++){
-            
-            emp[i]= "E" +(i);
+
+    public static String[] generarEmpleados(int n) {
+        String[] emp = new String[n];
+        for (int i = 0; i < n; i++) {
+            emp[i] = "E" + (i);
         }
         return emp;
     }
-    public static String [][] rubrosPreferencia (int n){
-       
-        String [][] rPref= new String [n][n];
-        for (int i=0;i<n;i++){
-            System.out.println("Rub "+i);
-            ArrayList a=generarSinRepetidos(n, i);
-            for (int j=0;j<n;j++){
-             
-                rPref[i][j]="E" + a.get(j).toString();
-                System.out.print(rPref[i][j].toString()+ " ");
+
+    public static String[][] rubrosPreferencia(int n) {
+
+        String[][] rPref = new String[n][n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Rub " + i);
+            ArrayList a = generarSinRepetidos(n, i);
+            for (int j = 0; j < n; j++) {
+
+                rPref[i][j] = "E" + a.get(j).toString();
+                System.out.print(rPref[i][j].toString() + " ");
             }
             System.out.println("");
-           
 
         }
         return rPref;
     }
-    public static String [][] empleadosPreferencia (int n){
-       
-        String [][] empPref= new String [n][n];
-        for (int i=0;i<n;i++){
-            System.out.println("Emp "+i);
-            ArrayList a=generarSinRepetidos(n, i);
-            for (int j=0;j<n;j++){
-                empPref[i][j]="R" +a.get(j).toString();
-                System.out.print(empPref[i][j].toString() +" ");
+
+    public static String[][] empleadosPreferencia(int n) {
+
+        String[][] empPref = new String[n][n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Emp " + i);
+            ArrayList a = generarSinRepetidos(n, i);
+            for (int j = 0; j < n; j++) {
+                empPref[i][j] = "R" + a.get(j).toString();
+                System.out.print(empPref[i][j].toString() + " ");
             }
             System.out.println("");
-           
 
         }
         System.out.println("---------------------------------------------------------------------------");
-        return  empPref;
+        return empPref;
     }
-    public static ArrayList generarSinRepetidos(int n, int p){
-        ArrayList a= new ArrayList<>();
-        Random r= new Random();
-        while (a.size()<n ){
-            int j=r.nextInt(n);
-            while (a.contains(j)){
-                j=r.nextInt(n);
+
+    public static ArrayList generarSinRepetidos(int n, int p) {
+        ArrayList a = new ArrayList<>();
+        Random r = new Random();
+        while (a.size() < n) {
+            int j = r.nextInt(n);
+            while (a.contains(j)) {
+                j = r.nextInt(n);
             }
             a.add(j);
         }
+        System.out.println("---------------------------------------------------------------------------");
         return a;
 
     }
-    
-    public static void main(String[] args) 
-    {
-       
-        Scanner leer= new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
         System.out.println(" Ingrese el n");
-        int n=leer.nextInt();
-        System.out.println("Ingrese 1 sea desea priorizar la motivacion de los empleados o 2 si prefiere priorizar la distribucion de recursos humanos");
-        int i=leer.nextInt();
-        //motivacion proponentes empleados
-        //distribucion proponentes rubbros
-        String [] proponente= new String [n];
-        String [] propuesto= new String [n];
-        String [][]propPref= new String [n][n];
-        String [][]propuestoPref= new String [n][n];
+        int n = leer.nextInt();
+        System.out.println(
+                "Ingrese 1 sea desea priorizar la motivacion de los empleados o 2 si prefiere priorizar la distribucion de recursos humanos");
+        int i = leer.nextInt();
+        // motivacion proponentes empleados
+        // distribucion proponentes rubbros
+        String[] proponente = new String[n];
+        String[] propuesto = new String[n];
+        String[][] propPref = new String[n][n];
+        String[][] propuestoPref = new String[n][n];
 
-        if (i==1){
-            proponente= generarEmpleados(n);
-            propuesto=generarRubros(n);
-            propPref=empleadosPreferencia(n);
-          
-            propuestoPref= rubrosPreferencia(n);
+        if (i == 1) {
+            proponente = generarEmpleados(n);
+            propuesto = generarRubros(n);
+            propPref = empleadosPreferencia(n);
+
+            propuestoPref = rubrosPreferencia(n);
+
+        } else {
+            proponente = generarRubros(n);
+            propuesto = generarEmpleados(n);
+            propPref = rubrosPreferencia(n);
+            propuestoPref = empleadosPreferencia(n);
 
         }
-        else{
-            proponente= generarRubros(n);
-            propuesto=generarEmpleados(n);
-            propPref= rubrosPreferencia(n);
-            propuestoPref=empleadosPreferencia(n);
-       
 
-        }
-       
-
- 
-        Algoritmo gs = new Algoritmo(proponente, propuesto, propPref, propuestoPref);                        
+        Algoritmo gs = new Algoritmo(proponente, propuesto, propPref, propuestoPref);
     }
 }
